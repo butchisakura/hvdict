2bb7a5a785368daa0a8208a9292341d1609ff0ad
vinhpham <vinh.pham@billbros.vn>
2021-10-14 17:11:49 +0700

    update config

--- docs/js/controller/HanVietDictCtrl.js
+++ docs/js/controller/HanVietDictCtrl.js
@@ -9,3 +9,27 @@ function HanVietDictCtrl($scope, $http)
 
     };
 }
+
+function render_hanzi(){
+    if (!window.HANZI_FIELD)
+    {
+        return;
+    }
+
+    HANZI_FIELD = window.HANZI_FIELD;
+    var characters = HANZI_FIELD.split("");
+    var js = document.createElement("script");
+    for (x of characters) 
+    {
+        var writer = HanziWriter.create('hanzi-target', x, {
+            width: 120,
+            height: 120,
+            padding: 5,  
+            showOutline: true,
+            strokeAnimationSpeed: 1,
+            delayBetweenStrokes: 150, // milliseconds
+            radicalColor: '#dc4e3f' // blue=#337ab7, red=#dc4e3f
+        });
+        writer.loopCharacterAnimation();
+    };
+}
