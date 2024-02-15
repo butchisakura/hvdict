a75e1a52e30861a80009d45a88bac4763f27515b
butchisakura <butchisakura@gmail.com>
2024-02-15 12:35:36 +0700

    refactor import, export 2 directions, update image

--- scripts/migration/main.py
+++ scripts/migration/main.py
@@ -4,6 +4,31 @@ import glob
 import re
 import csv
 import sys
+import base64
+
+HASH_WORD = "#word"
+HASH_STRUCT = "#struct"
+HASH_PRONUN = "#pronun"
+HASH_MEAN = "#mean"
+HASH_PICTURE = "#picture"
+HASH_NOTE = "#note"
+HASH_TAG = "#tag"
+
+SECTION_WORD = "## Word"
+SECTION_STRUCT = "## Cấu trúc"
+SECTION_PRONUN = "## Phát âm"
+SECTION_MEAN = "## Nghĩa"
+SECTION_PICTURE = "## Hình ảnh"
+SECTION_NOTE = "## Ghi chú"
+SECTION_TAG = "## Tags"
+
+def encode_base64(str):
+    # call decode to remove b''
+    return base64.b64encode(str.encode("utf-8")).decode("ascii")
+
+def decode_base64(str):
+    # call decode to remove b''
+    return base64.b64decode(str).decode("utf-8")
 
 def replace_chars(s):
     if not s:
@@ -40,10 +65,11 @@ def migrate_data():
     
     print("Generate internal data ...")
 
-    # Word | Structure | Pronun | Mean | Note | Tag
+    # Word | Pronun | Mean | Picture | Note | Tag | Structure
     for w in hv_words:
         lines = []
 
+        # Word
         lines.append("# " + w["name"])
         lines.append("")
 
@@ -51,13 +77,13 @@ def migrate_data():
 
         if w.get("pronunciation"):
             pronuns = w["pronunciation"].split(",")
-            lines.append("## Phát âm")
+            lines.append(SECTION_PRONUN)
             lines.extend(["* " + replace_chars(p) for p in pronuns if p])
             lines.append("")
 
         if w.get("meaning"):
             means = w["meaning"].split("\\r\\n")
-            lines.append("## Nghĩa")
+            lines.append(SECTION_MEAN)
 
             for m in means:
                 if not m:
@@ -86,7 +112,7 @@ def migrate_data():
 
         if w.get("category"):
             categories = w["category"].split(",")
-            lines.append("## Tags")
+            lines.append(SECTION_TAG)
             lines.extend(["* " + replace_chars(c) for c in categories if c])
             lines.append("")
 
@@ -125,15 +151,15 @@ def normalize_heading(filename, content):
 
         # migrate # => ##
         if re.match(r"^# Phát âm$", line):
-            line = line.replace("# Phát âm", "## Phát âm")
+            line = line.replace("# Phát âm", SECTION_PRONUN)
         if re.match(r"^# Nghĩa$", line):
-            line = line.replace("# Nghĩa", "## Nghĩa")
+            line = line.replace("# Nghĩa", SECTION_MEAN)
         if re.match(r"^# Tags$", line):
-            line = line.replace("# Tags", "## Tags")
+            line = line.replace("# Tags", SECTION_TAG)
         if re.match(r"^# Hình ảnh$", line):
-            line = line.replace("# Hình ảnh", "## Hình ảnh")
+            line = line.replace("# Hình ảnh", SECTION_PICTURE)
         if re.match(r"^# Ghi chú$", line):
-            line = line.replace("# Ghi chú", "## Ghi chú")
+            line = line.replace("# Ghi chú", SECTION_NOTE)
 
         # overwrite
         lines[idx] = line
@@ -182,11 +208,11 @@ def normalize_structure(filename, content):
 
     if len(structure_parts) > 0:
         tokens = []
-        tokens.append("## Cấu trúc")
+        tokens.append(SECTION_STRUCT)
         tokens.extend(structure_parts)
         tokens.append("")
 
-        targets = ["## Cấu trúc", "## Phát âm", "## Nghĩa", "## Tags"]
+        targets = [SECTION_STRUCT, SECTION_PRONUN, SECTION_MEAN, SECTION_TAG]
         for target in targets:
             if target in content:
                 tokens.append(target)
@@ -241,24 +267,24 @@ def normalize():
             fp.write(content)
         print("Done file ", filename)
 
-def get_section(lines, index):
+def get_section_hash(lines, index):
     idx = index
     while idx >= 0:
         line = lines[idx]
         if re.match(r"^#[^#]+", line):
-            return "#word"
-        if re.match(r"^## Cấu trúc", line):
-            return "#struct"
-        if re.match(r"^## Phát âm", line):
-            return "#pronun"
-        if re.match(r"^## Nghĩa", line):
-            return "#mean"
-        if re.match(r"^## Hình ảnh", line):
-            return "#picture"
-        if re.match(r"^## Ghi chú", line):
-            return "#note"
-        if re.match(r"^## Tags", line):
-            return "#tag"
+            return HASH_WORD
+        if re.match(r"^" + SECTION_STRUCT, line):
+            return HASH_STRUCT
+        if re.match(r"^" + SECTION_PRONUN, line):
+            return HASH_PRONUN
+        if re.match(r"^" + SECTION_MEAN, line):
+            return HASH_MEAN
+        if re.match(r"^" + SECTION_PICTURE, line):
+            return HASH_PICTURE
+        if re.match(r"^" + SECTION_NOTE, line):
+            return HASH_NOTE
+        if re.match(r"^" + SECTION_TAG, line):
+            return HASH_NOTE
         idx = idx - 1   # down from current line to 0
     return "#unknown"
 
@@ -270,9 +296,18 @@ def export_csv():
     md_files = glob.glob(working_dir + "/*.md")
     print("Number files %s" % len(md_files))
 
-    header_row = ["word", "pronun", "mean", "structure"]
-    rows = []
+    # Word | Pronun | Mean | Picture | Note | Tag | Structure
+    header_row = [
+       "#word",
+        HASH_PRONUN,
+        HASH_MEAN,
+        HASH_PICTURE,
+        HASH_NOTE,
+        HASH_TAG,
+        HASH_STRUCT
+    ]
 
+    rows = []
     for f in md_files:
         filename = os.path.basename(f)
         print("Reading file ", filename)
@@ -283,9 +318,11 @@ def export_csv():
         
         data_word = ""
         data_structure = ""
-        data_pronun = ""
-        data_mean = ""
-        data_means_parts = []
+        data_pronun_parts = []       
+        data_mean_parts = []
+        data_picture_parts = []
+        data_note_parts = []
+        data_tag_parts = []
 
         # 0:word 1:structure 2:mean
         section = 0
@@ -299,16 +336,16 @@ def export_csv():
             if lines[idx].startswith("<script"):
                 continue
 
-            section = get_section(lines, idx)
+            section_hash = get_section_hash(lines, idx)
 
-            # get one
-            if not data_word and section == "#word":
+            # Word: single
+            if not data_word and section_hash == HASH_WORD:
                 data_word = lines[idx][2:]
                 continue
 
-            # get one
-            if not data_structure and section == "#struct":
-                if lines[idx] == "## Cấu trúc":
+            # Struct: single
+            if not data_structure and section_hash == HASH_STRUCT:
+                if lines[idx] == SECTION_STRUCT:
                     continue
                 # * x = [y](y.md) [z](z.md) [t](t.md)
                 # re.findall(r"\[([^]]+)\]", data)
@@ -316,27 +353,58 @@ def export_csv():
                 data_structure = " ".join(parts)
                 continue
             
-            # get one
-            if not data_pronun and section == "#pronun":
-                if lines[idx] == "## Phát âm":
+            # Pronun: array
+            if section_hash == HASH_PRONUN:
+                if lines[idx] == SECTION_PRONUN:
                     continue
-                data_pronun = lines[idx][2:].replace("Hán Việt: ", "")
+                data_pronun_parts.append(lines[idx][2:])
                 continue
 
-            # get multiple
-            if section == "#mean":
-                if lines[idx] == "## Nghĩa":
+            # Mean: array
+            if section_hash == HASH_MEAN:
+                if lines[idx] == SECTION_MEAN:
                     continue
                 # accumulate item then re-overwrite
-                data_means_parts.append(lines[idx][2:])
-
-                # encode newline to safe for csv
-                data_mean = "\n".join(data_means_parts).replace("\n", "\\n")
+                data_mean_parts.append(lines[idx][2:])
                 continue
 
-        row = [data_word, data_pronun, data_mean, data_structure]
-        rows.append(row)
+            # Picture: array
+            if section_hash == HASH_PICTURE:
+                if lines[idx] == SECTION_PICTURE:
+                    continue
+                # accumulate item then re-overwrite
+                data_picture_parts.append(lines[idx][2:].replace("!", "'!"))
+                continue
+            
+            # Note: array
+            if section_hash == HASH_NOTE:
+                if lines[idx] == SECTION_NOTE:
+                    continue
+                # accumulate item then re-overwrite
+                data_note_parts.append(lines[idx][2:])
+                continue
 
+            # Tag: array
+            if section_hash == HASH_TAG:
+                if lines[idx] == SECTION_TAG:
+                    continue
+                # accumulate item then re-overwrite
+                data_tag_parts.append(lines[idx][2:])
+                continue
+
+
+        # Word | Pronun | Mean | Picture | Note | Tag | Structure
+        row = [
+            data_word, 
+            "\n".join(data_pronun_parts).replace("\n", "\\n"),
+            "\n".join(data_mean_parts).replace("\n", "\\n"), 
+            encode_base64("\n".join(data_picture_parts).replace("\n", "\\n")), 
+            "\n".join(data_note_parts).replace("\n", "\\n"), 
+            "\n".join(data_tag_parts).replace("\n", "\\n"), 
+            data_structure
+        ]
+        rows.append(row)
+    
     output_dir = os.path.join(current_dir, "output")
     if not os.path.exists(output_dir):
         print("creating output directory ", output_dir)
@@ -355,8 +423,7 @@ def export_csv():
     print("export done")
 
 def import_csv():
-    # headers: STT,Hán Việt,Phát âm,Nghĩa,Cấu trúc,0,1,2,3,4,5,6,7,8,9
-    # data: 4,時,thời,giờ,日 寺,日,寺,,,,,,,,
+    # headers: Date,Hán Việt,Phát âm,Nghĩa,Ghi chú,Tag,Cấu trúc,0,1,2,3,4,5,6,7,8,9
     current_dir = os.path.dirname(__file__)
     file_path = os.path.join(current_dir, "output", "import.csv")
     if not os.path.exists(file_path):
@@ -365,38 +432,76 @@ def import_csv():
     
     with open(file_path, "r", encoding="utf-8") as fp:
         rows = csv.reader(fp, delimiter=",")
-        row_idx = 0
+        row_idx = -1
         for row in rows:
+            row_idx = row_idx + 1
+            
             # ignore header
             if row_idx == 0:
+                print("ignore idx ", row_idx)
                 row_idx = row_idx + 1
                 continue
             
-            data_word = row[1]
+            # ignore empty line
+            if not row[1]:
+                print("ignore empty line, idx ", row_idx)
+                continue
 
+            # 0: date
+            # 1: word
+            # 2: pronun
+            # 3: mean
+            # 4: picture
+            # 5: note
+            # 6: tag
+            # 7: struct
             parts = []
+
+            data_word = row[1]
             parts.append("# " + data_word)
             parts.append("")
 
-            if row[4]:
-                parts.append("## Cấu trúc")
-                tokens = row[4].split(" ")
-                tokens = ["[%s](%s.md)" % (s.strip(), s.strip()) for s in tokens]
-                parts.append("* %s = %s" % (data_word, " ".join(tokens)))
-                parts.append("")
-
             if row[2]:
-                parts.append("## Phát âm")
-                tokens = row[2].split(",")
+                parts.append(SECTION_PRONUN)
+                tokens = row[2].split("\\n")
                 tokens = ["* Hán Việt: " + t.strip() for t in tokens]
                 parts.extend(tokens)
                 parts.append("")
 
             if row[3]:
-                parts.append("## Nghĩa")
-                tokens = row[3].split(",")
+                parts.append(SECTION_MEAN)
+                tokens = row[3].split("\\n")
                 tokens = ["* " + t.strip() for t in tokens]
                 parts.extend(tokens)
+                parts.append("")
+
+            if row[4]:
+                parts.append(SECTION_PICTURE)
+                picture_data = decode_base64(row[4])
+                tokens = picture_data.split("\\n")
+                tokens = ["* " + t.strip() for t in tokens if t.strip()]
+                parts.extend(tokens)
+                parts.append("")
+
+            if row[5]:
+                parts.append(SECTION_NOTE)
+                tokens = row[5].split("\\n")
+                tokens = ["* " + t.strip() for t in tokens]
+                parts.extend(tokens)
+                parts.append("")
+
+            if row[6]:
+                parts.append(SECTION_TAG)
+                tokens = row[6].split("\\n")
+                tokens = ["* " + t.strip() for t in tokens]
+                parts.extend(tokens)
+                parts.append("")
+
+            if row[7]:
+                parts.append(SECTION_STRUCT)
+                tokens = row[7].split(" ")
+                tokens = ["[%s](%s.md)" % (s.strip(), s.strip()) for s in tokens if s.strip()]
+                parts.append("* %s = %s" % (data_word, " ".join(tokens)))
                 parts.append("")
 
             parts.append("<script>window.HANZI_FIELD='%s';</script>" % data_word)
@@ -410,8 +515,7 @@ def import_csv():
                 wp.write(content)
             print("Import & output file ", export_filename)
 
-
-if __name__ == "__main__":
+def cli():
     if len(sys.argv) < 2:
         print("action is required: migrate | normalize | export | import")
         exit(1)
@@ -423,6 +527,9 @@ if __name__ == "__main__":
         "import": import_csv
     }
     action_mapping[action]()
+
+if __name__ == "__main__":
+    cli()
     # migrate_data()
     # normalize()
     # export_csv()
